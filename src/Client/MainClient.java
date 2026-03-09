e24a4e4eae9876a1997ce88a749d16415be2808e
FedeGove <federico.governatori07@gmail.com>
2026-03-09 12:49:57 +0100

    aggiunto server e client

--- src/Client/MainClient.java
+++ src/Client/MainClient.java
@@ -1,8 +1,37 @@
 package Client;
 
+import java.io.IOException;
+import java.net.*;
+
 public class MainClient {
     public static void main(String[] args) {
+        System.out.println("Client");
 
+        InetAddress serverAddress;
+        DatagramSocket dSocket;
+
+        int port = 3000;
+
+        try {
+            serverAddress = InetAddress.getLocalHost();
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            dSocket = new DatagramSocket();
+        } catch (SocketException e) {
+            throw new RuntimeException(e);
+        }
+
+        String message = "Ciao";
+        DatagramPacket dpo =  new DatagramPacket(message.getBytes(), message.length(), serverAddress, port);
+
+        try {
+            dSocket.send(dpo);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
 
--- src/Server/MainServer.java
+++ src/Server/MainServer.java
@@ -1,5 +1,17 @@
 package Server;
 
-public class MainServer {
+import java.net.DatagramSocket;
+import java.net.SocketException;
 
+public class MainServer {
+    public static void main(String[] args) {
+
+        int port = 3000;
+        System.out.println("SERVER INIZIATO");
+        try {
+            DatagramSocket dSocket = new DatagramSocket(port);
+        } catch (SocketException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
